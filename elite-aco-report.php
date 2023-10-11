ca6d9accc8b960d25736e2063f30df4e959497d1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-11 11:07:49 -0300

    11102302

--- elite-aco-report.php
+++ elite-aco-report.php
@@ -18,3 +18,29 @@
 require_once ELITE_ACO_REPORT_PATH . 'includes/functions.php';
 require_once ELITE_ACO_REPORT_PATH . 'admin/views/admin-view.php';
 
+// Funções de ativação e desativação
+register_activation_hook( __FILE__, 'elite_aco_report_activate' );
+register_deactivation_hook( __FILE__, 'elite_aco_report_deactivate' );
+
+function elite_aco_report_activate() {
+    // Código para ser executado na ativação do plugin
+}
+
+function elite_aco_report_deactivate() {
+    // Código para ser executado na desativação do plugin
+}
+
+// Adicionar link do plugin ao menu lateral do WP Admin
+add_action( 'admin_menu', 'elite_aco_report_menu' );
+
+function elite_aco_report_menu() {
+    add_menu_page(
+        'Relatórios Elite Aço',
+        'Elite Aço Report',
+        'manage_options',
+        'elite-aco-report/admin/views/admin-view.php',
+        '',
+        'dashicons-chart-pie',
+        6
+    );
+}
